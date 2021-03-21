f6679a45d8ba5aae5b72368e61bfe180eecc5d89
Kristen Groller <groller@rogers.com>
2021-03-21 15:34:38 -0400

    trying something new

--- models/Index.js
+++ models/Index.js
@@ -1,7 +1,7 @@
 // import all models
 const Post = require('./Post');
 const User = require('./User');
-const Vote = require('./Vote');
+//const Vote = require('./Vote');
 const Comment = require('./Comment');
 
 // create associations
@@ -14,7 +14,7 @@ Post.belongsTo(User, {
   onDelete: 'SET NULL'
 });
 
-User.belongsToMany(Post, {
+/*User.belongsToMany(Post, {
   through: Vote,
   as: 'voted_posts',
 
@@ -45,7 +45,7 @@ User.hasMany(Vote, {
 
 Post.hasMany(Vote, {
   foreignKey: 'post_id'
-});
+});*/
 
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
@@ -66,4 +66,4 @@ Post.hasMany(Comment, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote, Comment };
+module.exports = { User, Post, Comment };
